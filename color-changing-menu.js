c505e4b5fdc1f9b893468490d7b2a8d97ab7cc61
“Sara <sarapearce3.14@gmail.com>
2018-10-19 17:57:53 -0500

    changed colors to be inversions and high contrast against backgrounds, added in color changes for the other 6 sections

--- color-changing-menu.js
+++ color-changing-menu.js
@@ -1,26 +1,58 @@
 // Assumption: The size of each section is full screen
 jQuery(document).on('scroll', function(el) {
-  var windowHeight = parseInt(jQuery(window).height(), 10);
+  var windowHeight = jQuery(window).height();
   // section 1
   if (jQuery(document).scrollTop() < windowHeight) {
     jQuery(".hamburger-menu").css({
-      'color': '#ffffff'
+      'color': '#420f2f'
     });
-    console.log('Color White');
   }
   // section 2
   if (windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 2 * windowHeight) {
     jQuery(".hamburger-menu").css({
-      'color': '#000000'
+      'color': '#1b9a28'
     });
-    console.log('Color Black');
   }
   // section 3
   if (2 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 3 * windowHeight) {
     jQuery(".hamburger-menu").css({
+      'color': '#e7ff00'
+    });
+  }
+	// section 4: colors are already inversions in the background, go with white
+  if (3 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 4 * windowHeight) {
+    jQuery(".hamburger-menu").css({
       'color': '#ffffff'
     });
-    console.log('Color White again');
-
   }
+	// section 5
+  if (4 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 5 * windowHeight) {
+    jQuery(".hamburger-menu").css({
+      'color': '#620003'
+    });
+  }
+	// section 6
+  if (5 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 6 * windowHeight) {
+    jQuery(".hamburger-menu").css({
+      'color': '#ff70b3'
+    });
+  }
+	// section 7
+	if (6 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 7 * windowHeight) {
+		jQuery(".hamburger-menu").css({
+			'color': '#377d2a'
+		});
+	}
+	// section 8
+	if (7 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 8 * windowHeight) {
+		jQuery(".hamburger-menu").css({
+			'color': '#71b6ff'
+		});
+	}
+	// section 9: section contains both black and white, go with the inversion on the button color
+	if (8 * windowHeight < jQuery(document).scrollTop() && jQuery(document).scrollTop() < 9 * windowHeight) {
+		jQuery(".hamburger-menu").css({
+			'color': '#8c2800'
+		});
+	}
 })
